546f24d2fbea44cb2e24270d8c9cbc88f7af1b4c
angeluz142 <angeluz142@gmail.com>
2017-12-20 23:50:57 -0300

    new files

---
+++ DotNet/Async Await/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = new FileStream("./archivo.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine("Antes de Proocesar");
+
+            //Procesar(file);
+
+            ProcesarAsync(file);
+
+            Console.WriteLine("Despues de Proocesar");
+
+            Console.ReadLine();
+
+        }
+
+        private static void Procesar(FileStream file)
+        {
+            var msg = "Hola mundo";
+            var bytes = Encoding.UTF8.GetBytes(msg);
+
+            Console.WriteLine("Escribiendo");
+
+            for(int i = 0; i < 10000; i++)
+            {
+                file.Write(bytes,0,bytes.Length);
+            }
+            file.Close();
+        }
+
+        private static async void ProcesarAsync(FileStream file)
+        {
+            var msg = "Hola mundo";
+            var bytes = Encoding.UTF8.GetBytes(msg);
+
+            Console.WriteLine("Escribiendo...");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                await file.WriteAsync(bytes, 0, bytes.Length);
+            }
+            file.Close();
+            Console.WriteLine("! ! Ya escribio. ! !");
+        }
+    }
+}
---
+++ DotNet/Playing with Exceptions/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+             Otra forma seria usar using el cual realiza ejecuta implicitamente un
+             file.close y un finally; queda de la siguiente forma.
+
+               using (Stream f = File.Open("./File.txt", FileMode.Open))
+                    {
+                        try
+                        {
+                            int a = 10, b = 0;
+                            int res = a / b;
+                        }
+                        catch (DivideByZeroException) {
+                            Console.WriteLine("Division por cero.");
+                        }
+                        catch (Exception ex){
+                            Console.WriteLine($"Uups, ha ocurrido un error!: {ex.Message}");
+                        }
+
+                        Console.ReadLine();
+                    }
+
+             */
+
+
+
+
+            Stream f = null;
+            try
+            {
+                f = File.Open("./File.txt",FileMode.Open);
+                int a = 10, b = 0;
+                int res = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division por cero.");
+            }
+            catch (FileNotFoundException fileEx)
+            {
+                Console.WriteLine($"Error de archivo: {fileEx.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Uups, ha ocurrido un error!: {ex.Message}");
+            }
+
+            // Finally se ejecuta haya o no haya ocurrido alguna excepcion
+            finally
+            {
+                f.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
